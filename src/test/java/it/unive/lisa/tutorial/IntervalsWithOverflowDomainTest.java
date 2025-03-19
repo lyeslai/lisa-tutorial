8755d952769689af398cc4586eb20b30190c81ea
Julien Fang <62657775+Julien-Fang@users.noreply.github.com>
2025-03-19 10:13:55 +0100

    add IntervalsWithOverflowDomain imp and test file

---
+++ src/test/java/it/unive/lisa/tutorial/IntervalsWithOverflowDomainTest.java
@@ -0,0 +1,35 @@
+package it.unive.lisa.tutorial;
+
+import it.unive.lisa.AnalysisException;
+import it.unive.lisa.DefaultConfiguration;
+import it.unive.lisa.LiSA;
+import it.unive.lisa.analysis.nonrelational.value.ValueEnvironment;
+import it.unive.lisa.conf.LiSAConfiguration;
+import it.unive.lisa.conf.LiSAConfiguration.GraphType;
+import it.unive.lisa.imp.IMPFrontend;
+import it.unive.lisa.imp.ParsingException;
+import it.unive.lisa.program.Program;
+import org.junit.Test;
+
+public class IntervalsWithOverflowDomainTest {
+
+    @Test
+    public void testIntervalsWithOverflow() throws ParsingException, AnalysisException {
+        // Charger le fichier .imp
+        Program program = IMPFrontend.processFile("inputs/intervals.imp");
+        
+        // Configurer LiSA
+        LiSAConfiguration conf = new DefaultConfiguration();
+        conf.workdir = "outputs/intervals"; // Dossier de sortie
+        conf.analysisGraphs = GraphType.HTML; // Générer des graphes HTML
+        conf.abstractState = DefaultConfiguration.simpleState(
+            DefaultConfiguration.defaultHeapDomain(),
+            new ValueEnvironment(new IntervalsWithOverflowDomain()), // Ton domaine
+            DefaultConfiguration.defaultTypeDomain()
+        );
+        
+        // Lancer l'analyse
+        LiSA lisa = new LiSA(conf);
+        lisa.run(new Program[]{program});
+    }
+}
